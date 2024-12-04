58f01e14ac1d8ce91d3cd11e594ca3c6da410e08
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2024-12-04 07:30:54 +0530

    commit after a couple changes here and there

--- script.R
+++ script.R
@@ -1,11 +1,39 @@
-y <- data.frame(
-                age = seq(10, 60, by=10),
-                frequency = c(1, 2, 3, 5, 6, 4)
-);
-mat <- matrix(1:9, nrow=3, byrow=T);
-dimnames(mat) <- list(
-                      c("R1", "R2", "R3"),
-                      c("C1", "C2", "C3")
+# simple bar chart
+sales <- data.frame (
+                     year = 1996:2000,
+                     sales = c(15.0, 25.0, 27.0, 28.0, 26.0)
+                     )
+barplot(sales$sales, xlab="year", ylab="sales", main="Annual Sales", names.arg = sales$year)
+
+# for pie chart
+pivalues <- c(32, 87, 65, 29)
+pipercent <- round(100 * pivalues / sum(pivalues), 1)
+
+pie(
+    pivalues,
+    labels=pipercent,
+    col=rainbow(length(pivalues)),
+    main="Some distrobution"
+    )
+
+pilabels <- c("India", "USA", "U.K", "OTHER")
+legend(
+       "topright",
+       pilabels,
+       cex=1.0,
+       fill=rainbow(length(pivalues))
 )
 
-print(mat * 2);
+# histogram
+articles <- c(19, 23, 11, 5, 16, 21, 14, 19, 27, 39, 31)
+hist.articles <- hist(
+     articles, 
+     xlab="articles"
+)
+
+# frequency polygon
+axis.x <- c(min(hist.articles$breaks), hist.articles$mids, max(hist.articles$breaks))
+axis.y <- c(0, hist.articles$count, 0)
+
+lines(axis.x, axis.y, type="l")
+
