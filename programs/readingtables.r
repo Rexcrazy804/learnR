be7e2cefef481f7caf89dc5d29b4eb777117c71e
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-02-27 21:30:03 +0530

    reading tables

---
+++ programs/readingtables.r
@@ -0,0 +1,6 @@
+data1 <- read.csv("data/example.csv")
+data1 <- read.table("data/example.csv", header = TRUE, sep = ",")
+data2 <- read.table("data/example.txt", header = TRUE, sep = "\t")
+data2 <- read.delim("data/example.txt", header = TRUE)
+
+data1
