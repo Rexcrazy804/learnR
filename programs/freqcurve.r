95c2a16ebd95910e11601e0c8d39099054865a03
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-04-02 22:51:24 +0530

    some lint resolution + complete polygon prog

--- programs/freqcurve.r
+++ programs/freqcurve.r
@@ -2,6 +2,6 @@ library(ggplot2)
 set.seed(14012021)
 articles <- rnorm(200, mean = 4)
 ggplot(data.frame(x = articles), aes(x)) +
-  geom_histogram(aes(y = after_stat(density)), binwidth = 0.5, fill="pink", color="black") +
-  geom_density(adjust=1, color = "black") +
-  labs(title="Frequency Curve", x = "Value", y = "bob")
+  geom_histogram(aes(y = after_stat(density)), binwidth = 0.5, fill = "pink", color = "black") + # nolint: line_length_linter.
+  geom_density(adjust = 1, color = "black") +
+  labs(title = "Frequency Curve", x = "Value", y = "bob")
--- programs/freqpolygon.r
+++ programs/freqpolygon.r
@@ -1,4 +1,8 @@
-axis.x <- c(min(hist.articles$breaks), hist.articles$mids, max(hist.articles$breaks))
-axis.y <- c(0, hist.articles$count, 0)
+raw_data <- c(1, 2, 3, 4, 1, 2, 2, 3, 1, 1, 3, 4, 5, 5, 4, 3)
+break_points <- 0:length(unique(raw_data)) + 1
+tranformed_data <- cut(raw_data, break_points, right = FALSE)
+table_data <- table(tranformed_data)
+cum_freq <- c(0, cumsum(table_data))
 
-lines(axis.x, axis.y, type="l")
+plot(break_points, cum_freq)
+lines(break_points, cum_freq)
